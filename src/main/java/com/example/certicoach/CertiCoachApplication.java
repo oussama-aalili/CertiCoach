65bf9c384d0c1fadec223b7d529c6225a15d45f7
oussama-aalili <oussama.aalili@outlook.com>
2025-11-21 11:28:20 +0100

    Initial setup

---
+++ src/main/java/com/example/certicoach/CertiCoachApplication.java
@@ -0,0 +1,13 @@
+package com.example.certicoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertiCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertiCoachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/certicoach/CertiCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certicoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertiCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
