b3bd0d46ffa27f362cc03762330dd1092758e076
oussama-aalili <oussama.aalili@outlook.com>
2026-01-10 14:46:28 +0100

    stap 2.2 afgerond

---
+++ src/main/java/com/example/certicoach/model/Message.java
@@ -0,0 +1,26 @@
+package com.example.certicoach.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String content;
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String hello) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
