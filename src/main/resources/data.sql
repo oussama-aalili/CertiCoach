9a31387e03978c973172d942b5ebb7e549c764e7
oussama-aalili <oussama.aalili@outlook.com>
2026-01-10 14:48:05 +0100

    stap 2.3 afgerond

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO message (id, content) VALUES (1, 'Hello');
+INSERT INTO message (id, content) VALUES (2, 'Welkom bij CertiCoach');
+INSERT INTO message (id, content) VALUES (3, 'Goed bezig!');
